9c2048a05a7d9802373999a671efa2042bdffe74
root <tpdnrzz@jungbu.ac.kr>
2016-04-23 09:11:01 -0700

    modify

--- main.cpp
+++ main.cpp
@@ -1,87 +1,11 @@
 #include <pcap/pcap.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-int eth_hdr = 0;
-int ip_hdr = eth_hdr + 12;
-int tcp_hdr = ip_hdr + 11;
-int udp_hdr = ip_hdr + 11;
-
-void get_pcap(u_char *args, const struct pcap_pkthdr *header, const u_char *p)
-{
+#include <libnet.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <netinet/udp.h>
+#include <netinet/in.h>
 
 
-    int des, src;
-
-    /*============ Ethernet ==========*/
-    printf("    <Ethernet> - src mac: ");
-    for(src = eth_hdr + 6; src < eth_hdr + 12 ; src++)
-    {
-        if(src == 11)printf("%02X", p[src]);
-        else printf("%02X:", p[src]);
-    }
-    printf("\n");
-
-    printf("    <Ethernet> - dst mac: ");
-    for(des = eth_hdr; des < eth_hdr + 6; des++)
-    {
-        if(des == 5)printf("%02X", p[des]);
-        else printf("%02X:", p[des]);
-    }
-    printf("\n\n\n");
-
-    /*========== IP ===========*/
-    if(p[ip_hdr] == 8 && p[ip_hdr+1] == 0) //p[14] = Header Length
-    printf("    <IP> - src ip: ");
-
-    for(src = ip_hdr + 14; src < ip_hdr + 18; src++) //src ip: 26~29
-    {
-        if(src == 29)printf("%d", p[src]);
-        else printf("%d.", p[src]);
-    }
-    printf("\n");
-
-    printf("    <IP> - dst ip: ");
-    for(des = ip_hdr + 18; des < ip_hdr + 22; des++) // dst ip: 30 ~ 33
-    {
-        if(des == 33)printf("%d", p[des]);
-        else printf("%d.", p[des]);
-    }
-    printf("\n\n\n");
-
-    /*========== TCP ===========*/
-
-
-    if(p[tcp_hdr] == 6) // p[23] == 6 TCP port, tcp_hdr = 23
-         {
-            printf("    <TCP> - src prt: ");
-
-            //strcat(p[tcp_hdr + 11], p[tcp_hdr + 12]);
-
-            printf("%02X:", p[tcp_hdr + 11]); // --> 34
-            printf("%02X", p[tcp_hdr + 12]); // --> 35
-            printf("\n");
-
-            printf("    <TCP> - dst prt: ");
-            printf("%02X:", p[tcp_hdr + 13]); // --> 36
-            printf("%02X", p[tcp_hdr + 14]); // --> 37
-            printf("\n\n");
-         }
-
-    if(p[udp_hdr] == 13) // p[23] == 6 TCP port, tcp_hdr = 23
-         {
-            printf("    <UDP> - src prt: ");
-            printf("%02X:", p[udp_hdr + 11]); // --> 34
-            printf("%02X", p[udp_hdr + 12]); // --> 35
-            printf("\n");
-
-            printf("    <UDP> - dst prt: ");
-            printf("%02X:", p[udp_hdr + 13]); // --> 36
-            printf("%02X", p[udp_hdr + 14]); // --> 37
-            printf("\n\n");
-         }
-}
 
 
 
@@ -107,14 +31,74 @@ int main()
           return(2);
       }
 
-      pcap_loop(handle, -1, get_pcap, NULL);
+      while(1){
+                          struct pcap_pkthdr *h;
+                          const u_char * p;
+                          int res = pcap_next_ex(handle, &h, &p);
+                          if(res == -1) break;
 
+
+                          struct libnet_ethernet_hdr *ehP = (struct libnet_ethernet_hdr *)p;
+
+                          /*===========  get Src Mac, Dst Mac(eth0) ==========*/
+
+                          printf("                  ** eth0 **\n");
+                          printf("      Source MAC     ->  Destination MAC\n");
+                          printf("   %02X:%02X:%02X:%02X:%02X:%02X -> %02X:%02X:%02X:%02X:%02X:%02X\n\n\n",
+                                      ehP->ether_shost[0],
+                                      ehP->ether_shost[1],
+                                      ehP->ether_shost[2],
+                                      ehP->ether_shost[3],
+                                      ehP->ether_shost[4],
+                                      ehP->ether_shost[5],
+                                      ehP->ether_dhost[0],
+                                      ehP->ether_dhost[1],
+                                      ehP->ether_dhost[2],
+                                      ehP->ether_dhost[3],
+                                      ehP->ether_dhost[4],
+                                      ehP->ether_dhost[5]);
+
+                         /*===========  get Src ip, Dst ip(IP) ==========*/
+
+
+                          if(ntohs(ehP->ether_type) == ETHERTYPE_IP)
+                            {
+                                struct libnet_ipv4_hdr* ihP;
+                                ihP = (struct libnet_ipv4_hdr*)(p + sizeof(*ehP));
+
+                                printf("               ** EHTERTYPE_IP **\n");
+                                printf("      Source IP     ->    Destination IP   \n");
+                                printf("     %s     ", inet_ntoa(ihP->ip_src));
+                                printf("-> %15s    \n\n\n", inet_ntoa(ihP->ip_dst));
+
+                                /*===========  get Src, Dst port(TCP) ==========*/
+
+                                if(ihP->ip_p == IPPROTO_TCP){
+                                struct libnet_tcp_hdr *tcph;
+                                tcph = (struct libnet_tcp_hdr *)(p + 34); //34 + TCP/UDP header palce
+                                printf("               ** TCP Information **\n");
+                                printf("     Src Port : %d\n" , ntohs(tcph->th_sport));
+                                printf("     Dst Port : %d\n" , ntohs(tcph->th_dport));
+                                /*printf("%d\n", ihP->ip_hl * 4);*/
+                                }
+
+                                /*===========  get Src, Dst port(UDP) ==========*/
+
+                                else if(ihP->ip_p == IPPROTO_UDP)
+                                {
+                                    struct libnet_udp_hdr *udph;
+                                    udph = (struct libnet_udp_hdr *)(p + 34); //34 + TCP/UDP header palce
+                                    printf("               ** UDP Information **\n");
+                                    printf("     Src Port : %d\n" , ntohs(udph->uh_sport));
+                                    printf("Dst Port : %d\n" , ntohs(udph->uh_sport));
+                                }
+                            }
+
+               }
       pcap_close(handle);
 
    return(0);
 
 }
-
-//http://egloos.zum.com/innaei/v/4416421
 
 
